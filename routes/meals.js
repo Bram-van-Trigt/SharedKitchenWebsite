a61c34bc4fe3aa9aced1acc0327340950b616018
Bram van Trigt <bramvantrigt@gmail.com>
2021-07-20 14:53:18 +0200

    My meals API returns json file as string

--- routes/meals.js
+++ routes/meals.js
@@ -3,16 +3,12 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-var meals = fs.readFile('tempJson/myMeals.json', 'utf-8', (err, data) =>{
-    if (err) {
-        console.error(err);
-        return
-    }
-    else console.log(data);
-});
+var myMeals = fs.readFileSync('tempJson/myMeals.json', 'utf-8');
+
+console.log(myMeals);
 
 router.get('/API', function( req, res, next ) {
-    res.render('API', { title: 'Meals API', header: 'Meals API:', text: meals });
+    res.render('API', { title: 'Meals API', header: 'Meals API:', text: myMeals });
 });
 
 module.exports = router;
