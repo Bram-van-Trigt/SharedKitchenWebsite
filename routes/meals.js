9eb54788ddbf075c0f4fde4d1ac37cd8ab7b2ce8
Bram van Trigt <bramvantrigt@gmail.com>
2021-07-20 14:14:32 +0200

    Implementing meals API

--- routes/meals.js
+++ routes/meals.js
@@ -1,9 +1,18 @@
+const { json } = require('express');
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
+var meals = fs.readFile('tempJson/myMeals.json', 'utf-8', (err, data) =>{
+    if (err) {
+        console.error(err);
+        return
+    }
+    else console.log(data);
+});
 
 router.get('/API', function( req, res, next ) {
-    res.render('index', { title: 'Hey', message: 'hello there!'});
+    res.render('API', { title: 'Meals API', header: 'Meals API:', text: meals });
 });
 
 module.exports = router;
