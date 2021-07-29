f93c6c68327036af31beb76fbb0448939c225276
Bram van Trigt <bramvantrigt@gmail.com>
2021-07-29 17:03:28 +0200

    Meals are displayed in table

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors())
 
 // view engine setup
 app.set('view engine', 'pug');
-// app.use(express.static(path.join(__dirname , 'public')));         outcomment to check what solved error
+app.use(express.static(path.join(__dirname , 'public')));
 app.set('views',path.join(__dirname , 'views'));
 
 
--- routes/meals.js
+++ routes/meals.js
@@ -3,17 +3,15 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-// read json file
-var myMeals = fs.readFileSync('tempJson/myMeals.json', 'utf-8');
-console.log(myMeals);
-
 // render routes
 router.get('/', function( req, res, next ) {
-    var meal_list = JSON.parse(myMeals);
-    res.render('myMeals', { title: 'My meals', header: 'Available meals:', text: meal_list.meals[0].recipe });
+    var myMeals = JSON.parse(fs.readFileSync('tempJson/myMeals.json', 'utf-8'));
+    console.log(myMeals);
+    res.render('myMeals', {title:'My meals', header:'AVAILABLE MEALS:', tableInput: myMeals.meals});
 });
 
 router.get('/API', function( req, res, next ) {
+    var myMeals = fs.readFileSync('tempJson/myMeals.json', 'utf-8');
     res.render('API', { text: myMeals });
 });
 
